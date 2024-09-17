f1b5388a3bbd8c2a8dee4c72986733855fae5773
jkha21 <hkumar3516@gmail.com>
2024-09-17 13:02:11 +0530

    [harsh]added: Json server

--- js/empPayrollRegistration.js
+++ js/empPayrollRegistration.js
@@ -10,7 +10,13 @@ const yearRef = document.getElementById('year-cnt');
 const notesRef = document.getElementById('notes-cnt');
 const resetBtnRef = document.getElementById("resetButton");
 const cancelBtnRef = document.getElementById("cancelButton");
-
+let nameErrorlabel = document.getElementById('empDash-nullValuecheck-cnt');
+let checkProfile = document.getElementById('empDash-checkProfile-cnt');
+let checkDepartment = document.getElementById('empDash-checkDepartment-cnt');
+let checkGender = document.getElementById('empDash-checkGender-cnt');
+let checkSalary = document.getElementById('empDash-checkSalary-cnt');
+let checkDate = document.getElementById('empDash-checkStartDate-cnt');
+let checkNotes = document.getElementById('empDash-checkNotes-cnt');
 
 function resetform(){
     nameRef.value = "";
@@ -27,26 +33,78 @@ function resetform(){
 submitBtnRef.addEventListener('click', (e) =>{
     e.preventDefault();
     const nameValue = nameRef.value;
+    if(!nameValidator(nameValue)){
+        nameErrorlabel.textContent = "Wrong Input! Enter the name again.";
+        nameErrorlabel.style.color = 'red';
+        nameErrorlabel.style.fontSize = '14px';
+        nameErrorlabel.style.fontWeight = 'bold';
+        return;        
+    }else{
+        nameErrorlabel.textContent = "";
+    }
+    
     let selectedProfile = null;
     for(let element of profileRef){
         if(element.checked){
-            selectedProfile = element.nextElementSibling.getAttribute('src')
+            selectedProfile = element.nextElementSibling.getAttribute('src');
+        }
+        if(!selectedProfile){
+            checkProfile.textContent = "Error! Select Profile";
+            checkProfile.style.color = 'red';
+            checkProfile.style.fontSize = '14px';
+            checkProfile.style.fontWeight = 'bold';
+            return;
+        }else{
+            checkProfile.textContent = "";
         }
     }
+    
     let selectedGender = null;
     for(let element of genderRef){
         if(element.checked){
             selectedGender = element.nextElementSibling.textContent;
         }
+        if(!selectedGender){
+            checkGender.textContent = "Error! Select Gender"; 
+            checkGender.style.color = 'red';
+            checkGender.style.fontSize = '14px';
+            checkGender.style.fontWeight = 'bold';
+            return;
+        }else{
+            checkGender.textContent = ""; 
+        }
     };
-
-    let selectedSalary = salaryRef.value;
+    
+    
+    
     let selectedDepartmentObj = [];
     for(let element of departmentRef){
         if(element.checked){
             selectedDepartmentObj.push(element.nextElementSibling.textContent);
         }
+        if(!selectedDepartmentObj.length){
+            checkDepartment.textContent = 'Error! Select the department';
+            checkDepartment.style.color = 'red';
+            checkDepartment.style.fontSize = '14px';
+            checkDepartment.style.fontWeight = 'bold';
+            return;
+        }else{
+            checkDepartment.textContent = "";
+        }
     }
+
+
+    let selectedSalary = salaryRef.value;
+    if(salaryRef.selectedIndex === 0){
+        checkSalary.textContent = "Error! Select Salary Range"
+        checkSalary.style.color = 'red';
+        checkSalary.style.fontSize = '14px';
+        checkSalary.style.fontWeight = 'bold';
+        return;
+    }else{
+        checkSalary.textContent = "";
+    }
+
 
     let dateObj = {
         date: dateRef.value,
@@ -54,8 +112,27 @@ submitBtnRef.addEventListener('click', (e) =>{
         year: yearRef.value
     }
     
+    if(dateRef.selectedIndex === 0 || monthRef.selectedIndex === 0 || yearRef.selectedIndex === 0){
+        checkDate.textContent = "Error! Fill the Start Date";
+        checkDate.style.color = 'red';
+        checkDate.style.fontSize = '14px';
+        checkDate.style.fontWeight = 'bold';
+        return;
+    }else{
+        checkDate.textContent = "";
+    }
     
     let notesContent = notesRef.value;
+    if(!notesContent){
+        checkNotes.textContent = "Error! Notes are empty";
+        checkNotes.style.color = 'red';
+        checkNotes.style.fontSize = '14px';
+        checkNotes.style.fontWeight = 'bold';
+        return;
+    }else{
+        checkNotes.textContent = ""
+    }
+    
     const empObj = {
         name: nameValue,
         profile: selectedProfile,
@@ -66,12 +143,21 @@ submitBtnRef.addEventListener('click', (e) =>{
         notes: notesContent
     }
     
-    let empRecordList = JSON.parse(localStorage.getItem('empDataList'))
-    empRecordList?.length > 0 ? localStorage.setItem("empDataList", JSON.stringify(empObj)):
-    localStorage.setItem("empDataList", JSON.stringify(empObj));
-    empRecordList = localStorage.getItem('empDataList');
-    console.log(empObj)
-    resetform();
+    
+    $.ajax({
+        url: 'http://localhost:3001/employees',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(empObj),
+        success: function() {
+            console.log('Data saved successfully');
+            resetform();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('Error:', textStatus, errorThrown);
+        }
+    });
+        
 });
 
 
@@ -87,6 +173,12 @@ cancelBtnRef.addEventListener('click', (e)=>{
     e.preventDefault();
     resetform();
 })
+
+
+
+function nameValidator(name){
+    return /^[A-Z][a-z]*(?: [A-Z][a-z]*)*$/.test(name);
+}
 
 
 
